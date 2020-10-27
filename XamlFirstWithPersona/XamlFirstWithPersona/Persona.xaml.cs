c13a84c9b9b9c2346e8049d38d7a3f4921a3ea92
Jordan Mele <jordan.mele@outlook.com.au>
2020-10-28 10:14:30 +1100

    Add project files.

---
+++ XamlFirstWithPersona/XamlFirstWithPersona/Persona.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamlFirstWithPersona
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Persona : ContentView
+    {
+        public Persona()
+        {
+            InitializeComponent();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return (string)this.GetValue(Persona.NameProperty);
+            }
+            set
+            {
+                this.SetValue(Persona.NameProperty, value);
+            }
+        }
+
+        public static readonly BindableProperty NameProperty = BindableProperty.Create(
+            propertyName: nameof(Persona.Name),
+            returnType: typeof(string),
+            declaringType: typeof(Persona));
+
+        protected override void OnPropertyChanged(string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+            if (propertyName == Persona.NameProperty.PropertyName)
+            {
+                this.NameLabel.Text = this.Name;
+            }
+        }
+    }
+}
